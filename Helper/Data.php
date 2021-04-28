99bd1e40dab0bf5957242da8f5f4a9d4d3f542ec
xuantung <tungdx@smartosc.com>
2021-04-28 14:56:14 +0700

    First version 21.04.28

---
+++ Helper/Data.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace SM\Stripe\Helper;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Module\ModuleListInterface;
+use Magento\Store\Model\ScopeInterface;
+use SM\Payment\Model\ResourceModel\RetailPayment\CollectionFactory;
+use SM\XRetail\Helper\Data as RetailHelper;
+
+class Data
+{
+    /**
+     * @var RetailHelper
+     */
+    protected $retailHelper;
+    /**
+     * @var ModuleListInterface
+     */
+    protected $moduleList;
+    /**
+     * @var CollectionFactory
+     */
+    protected $paymentCollectionFactory;
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    public function __construct(
+        RetailHelper $retailHelper,
+        ModuleListInterface $moduleList,
+        CollectionFactory $paymentCollectionFactory,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->retailHelper = $retailHelper;
+        $this->moduleList = $moduleList;
+        $this->paymentCollectionFactory = $paymentCollectionFactory;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return bool
+     * @throws \Exception
+     */
+    public function checkLibraryExists()
+    {
+        if (!class_exists(\Stripe\Stripe::class)) {
+            throw new \Exception(
+                __("The Stripe PHP library dependency has not been installed. Please follow the installation instructions at https://stripe.com/docs/plugins/magento/install#manual")
+            );
+        }
+        return true;
+    }
+
+    /**
+     * $type = secret or publishable
+     * @param string $type
+     * @return false|mixed
+     */
+    public function getApiKey($type, $storeId = null)
+    {
+        // get API Key from config
+        if ($this->moduleList->getOne('StripeIntegration_Payments')) {
+            $stripeMode = $this->getConfigValue('payment/stripe_payments_basic/stripe_mode', $storeId);
+            if ($type === 'secret') {
+                $apiKey = $this->getConfigValue("payment/stripe_payments_basic/stripe_{$stripeMode}_sk", $storeId);
+            } else {
+                $apiKey = $this->getConfigValue("payment/stripe_payments_basic/stripe_{$stripeMode}_pk", $storeId);
+            }
+            if ($apiKey) {
+                return $apiKey;
+            }
+        }
+        $collection = $this->paymentCollectionFactory->create()
+            ->addFieldToFilter('type', ['eq' => \SM\Payment\Model\RetailPayment::STRIPE]);
+        if ($collection->count() == 0) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('Payment not found!'));
+        }
+        $stripePayment = $collection->getFirstItem();
+        $paymentData = json_decode($stripePayment->getData('payment_data'), true);
+
+        if ($type === 'secret') {
+            return $paymentData['secret_api_key'];
+        }
+        return $paymentData['publishable_api_key'];
+    }
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+}
---
+++ Repositories/StripeManagement.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace SM\Stripe\Repositories;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use SM\Stripe\Helper\Data;
+use SM\XRetail\Helper\DataConfig;
+use SM\XRetail\Repositories\Contract\ServiceAbstract;
+use Stripe\Stripe;
+
+class StripeManagement extends ServiceAbstract
+{
+    /**
+     * @var Data
+     */
+    protected $stripeHelper;
+    /**
+     * @var CartRepositoryInterface
+     */
+    protected $cartRepository;
+
+    public function __construct(
+        RequestInterface $requestInterface,
+        DataConfig $dataConfig,
+        StoreManagerInterface $storeManager,
+        Data $stripeHelper,
+        CartRepositoryInterface $cartRepository
+    ) {
+        parent::__construct($requestInterface, $dataConfig, $storeManager);
+        $this->stripeHelper = $stripeHelper;
+        $this->cartRepository = $cartRepository;
+    }
+
+    /**
+     * @return $this
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function initStripe()
+    {
+        $this->stripeHelper->checkLibraryExists();
+        $storeId = $this->getRequest()->getParam('store_id');
+        $apiKey = $this->stripeHelper->getApiKey('secret', $storeId);
+        if (!$apiKey) {
+            throw new \Exception(__('Please configure the API Key first.'));
+        }
+
+        Stripe::setApiKey($apiKey);
+        return $this;
+    }
+
+    /**
+     * @param $quoteId
+     * @return float
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    protected function getTotalAmountFromQuote($quoteId)
+    {
+        $quote = $this->cartRepository->get($quoteId);
+        return (int) $quote->getGrandTotal() * 100;
+    }
+
+    /**
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function createPaymentIntent()
+    {
+        $this->initStripe();
+        $quoteId = $this->getRequest()->getParam('quote_id');
+
+        try {
+            $paymentIntent = \Stripe\PaymentIntent::create(
+                [
+                    'amount' => $this->getTotalAmountFromQuote($quoteId),
+                    'currency' => "usd",
+                    'payment_method_types' => ['card_present'],
+                    'capture_method' => 'manual',
+                ]
+            );
+
+            return $this->getSearchResult()
+                ->setItems([$paymentIntent->client_secret])
+                ->setErrors([])
+                ->setTotalCount(1)
+                ->setLastPageNumber(1)
+                ->getOutput();
+        } catch (\Exception $e) {
+            throw new \Exception(__($e->getMessage()));
+        }
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function getConnectionToken()
+    {
+        $this->initStripe();
+        try {
+            $token = \Stripe\Terminal\ConnectionToken::create();
+
+            return $this->getSearchResult()
+                ->setItems([$token->secret])
+                ->setErrors([])
+                ->setTotalCount(1)
+                ->setLastPageNumber(1)
+                ->getOutput();
+        } catch (\Exception $e) {
+            throw new \Exception(__($e->getMessage()));
+        }
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function capturePayment()
+    {
+        $this->initStripe();
+        $intentId = $this->getRequest()->getParam('intent_id');
+        try {
+            $paymentIntent = \Stripe\PaymentIntent::retrieve($intentId);
+            $paymentIntent->capture();
+
+            return $this->getSearchResult()
+                ->setItems([$paymentIntent->status])
+                ->setErrors([])
+                ->setTotalCount(1)
+                ->setLastPageNumber(1)
+                ->getOutput();
+        } catch (\Exception $e) {
+            throw new \Exception(__($e->getMessage()));
+        }
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function refundPayment()
+    {
+        $this->initStripe();
+        $intentId = $this->getRequest()->getParam('intent_id');
+        $amount = $this->getRequest()->getParam('amount');
+        try {
+            if ($amount) {
+                $refund = \Stripe\Refund::create(
+                    ['payment_intent' => $intentId, 'amount' => $amount]
+                );
+            } else {
+                $refund = \Stripe\Refund::create(
+                    ['payment_intent' => $intentId]
+                );
+            }
+            return $this->getSearchResult()
+                ->setItems([$refund->status])
+                ->setErrors([])
+                ->setTotalCount(1)
+                ->setLastPageNumber(1)
+                ->getOutput();
+        } catch (\Exception $e) {
+            throw new \Exception(__($e->getMessage()));
+        }
+    }
+}
